de6e09fd4b6d6d091eb62e83209377e6bbd918ef
Alejandro <gato.park26@hotmail.com>
2025-04-01 15:17:41 -0300

    mi archivo

---
+++ mi-archivo2.py
@@ -0,0 +1,4 @@
+nombre = input("cual es tu nombre?")
+apellido = input("cual es tu apellido?")
+provincia = input("de que provincia sos?")
+print (f"Me llamo {nombre} {apellido} y soy de {provincia}")
